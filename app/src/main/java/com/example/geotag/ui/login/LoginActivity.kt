c02250e331d7e85b7f08c7227e11b2a1875be41a
ikram <you@example.com>
2023-12-12 02:04:45 +0800

    menambahkan logic untuk login

--- app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
@@ -5,26 +5,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import com.example.geotag.ui.main.MainActivity
 import com.example.geotag.R
 import com.example.geotag.ui.welcome.WelcomeActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailLayout: TextInputLayout
+    private lateinit var passwordLayout: TextInputLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val backButton = findViewById<ImageButton>(R.id.back_btn)
         val loginBtn = findViewById<Button>(R.id.btn_login)
+        emailLayout = findViewById(R.id.layout_email)
+        passwordLayout = findViewById(R.id.layout_password)
 
         backButton.setOnClickListener {
             // Handle the back button click
             navigateBackToWelcomeActivity()
         }
 
-        loginBtn.setOnClickListener {
-            // Handle the login button click
-            navigateToHomeActivity()
+        val loginButton: Button = findViewById(R.id.btn_login)
+        loginButton.setOnClickListener {
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        val email = emailLayout.editText?.text.toString()
+        val password = passwordLayout.editText?.text.toString()
+
+        // Perform your login logic here
+        // You can add validation and authentication code
+
+        // For example, displaying a toast message
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Please enter valid email and password", Toast.LENGTH_SHORT).show()
         }
     }
 
