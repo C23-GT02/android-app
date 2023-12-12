bd4fe3ff69db337672f71041c74539cd25d00771
Bananalem0n <ukasyahnata@outlook.com>
2023-12-13 03:48:15 +0700

    add login function set okHttpClient to catch cookie header

--- app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
@@ -13,7 +13,6 @@ import com.example.geotag.data.models.LoginRequest
 import com.example.geotag.data.response.LoginResponse
 import com.example.geotag.data.retrofit.apiService
 import com.example.geotag.data.retrofit.fetch
-import com.example.geotag.ui.main.MainActivity
 import com.example.geotag.ui.welcome.WelcomeActivity
 import com.google.android.material.textfield.TextInputLayout
 
@@ -27,9 +26,6 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
 
 
-    private lateinit var emailLayout: TextInputLayout
-    private lateinit var passwordLayout: TextInputLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -38,11 +34,6 @@ class LoginActivity : AppCompatActivity() {
         passwordLayout = findViewById(R.id.input_password)
         backButton = findViewById(R.id.back_btn)
         loginButton = findViewById(R.id.btn_login)
-
-        val backButton = findViewById<ImageButton>(R.id.back_btn)
-        val loginBtn = findViewById<Button>(R.id.btn_login)
-        emailLayout = findViewById(R.id.layout_email)
-        passwordLayout = findViewById(R.id.layout_password)
 
 
         backButton.setOnClickListener {
