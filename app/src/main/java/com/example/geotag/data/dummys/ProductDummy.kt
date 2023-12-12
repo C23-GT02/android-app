304319f1de890e47e032aa674852488de7def841
ikram <you@example.com>
2023-12-12 17:40:23 +0800

    menambahkan logic untuk product dummy, product adapter, menambahkan dummy image, menambahkan card view, dan menginisialisasi cardview ke product detail xml

--- app/src/main/java/com/example/geotag/data/dummys/ProductDummy.kt
+++ app/src/main/java/com/example/geotag/data/dummys/ProductDummy.kt
@@ -1,7 +1,36 @@
 package com.example.geotag.data.dummys
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ProductDummy(
     val productTitle: String,
     val productDescription: String,
     val productLogo: Int
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(productTitle)
+        parcel.writeString(productDescription)
+        parcel.writeInt(productLogo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProductDummy> {
+        override fun createFromParcel(parcel: Parcel): ProductDummy {
+            return ProductDummy(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProductDummy?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
