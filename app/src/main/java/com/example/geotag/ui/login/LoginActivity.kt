6d971dadfd5fce71e130c74b5fa821a3ee1d6a29
Bananalem0n <ukasyahnata@outlook.com>
2023-12-13 03:48:15 +0700

    add login function set okHttpClient to catch cookie header

--- app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/geotag/ui/login/LoginActivity.kt
@@ -1,36 +1,37 @@
 package com.example.geotag.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
-import com.example.geotag.ui.main.MainActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.geotag.R
-import com.example.geotag.data.models.LoginModel
+import com.example.geotag.data.models.LoginRequest
 import com.example.geotag.data.response.LoginResponse
 import com.example.geotag.data.retrofit.apiService
 import com.example.geotag.data.retrofit.fetch
+import com.example.geotag.ui.main.MainActivity
 import com.example.geotag.ui.welcome.WelcomeActivity
-import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var emailLayout: TextInputLayout
-    private lateinit var passwordLayout: TextInputLayout
+    private lateinit var emailLayout: EditText
+    private lateinit var passwordLayout: EditText
     private lateinit var backButton: ImageButton
     private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        emailLayout = findViewById(R.id.layout_email)
-        passwordLayout = findViewById(R.id.layout_password)
+        
+        emailLayout = findViewById(R.id.input_email)
+        passwordLayout = findViewById(R.id.input_password)
         backButton = findViewById(R.id.back_btn)
         loginButton = findViewById(R.id.btn_login)
+
 
         backButton.setOnClickListener {
             // Handle the back button click
@@ -43,10 +44,10 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun login () {
-        val email = emailLayout.editText.toString()
-        val password = passwordLayout.editText.toString()
+        val email = emailLayout.text.toString()
+        val password = passwordLayout.text.toString()
 
-        val loginRequest = LoginModel(email, password)
+        val loginRequest = LoginRequest(email, password)
         val call: Call<LoginResponse> = apiService.loginUser(loginRequest)
 
         fetch(call,
@@ -65,14 +66,14 @@ class LoginActivity : AppCompatActivity() {
         )
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        // Override the back button press
-        navigateBackToWelcomeActivity()
-    }
-
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        // Handle the back button press
+        navigateBackToWelcomeActivity()
     }
 
     private fun navigateBackToWelcomeActivity() {
